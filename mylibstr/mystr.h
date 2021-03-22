d3637ae791cc615b0e75d125a45bb33cfe06858b
benji-35 <benjamin.delvert@epitech.eu>
2021-03-22 11:19:39 +0100

    CHG : add comments in header

--- mylibstr/mystr.h
+++ mylibstr/mystr.h
@@ -173,28 +173,145 @@ string double_to_str(double f, int precision);
 double str_to_double(string str);
 
 /**
- * reverse string
+ * reverse the string
  * @param str > string pointer
  */
 void reverse_str(string *str);
+
+/**
+ * add new line with str in text at specific line
+ * @param txt > text pointer
+ * @param pos > line in text
+ */
 void add_line_ptext(text *txt, int pos);
+
+/**
+ * add empty str in text at end
+ * @param txt > text pointer
+ */
 void add_line_text(text *txt);
+
+/**
+ * add str in text at specific line
+ * @param txt > text pointer
+ * @param str > string
+ * @param pos > line in text
+ */
 void add_str_text(text *txt, int pos, string str);
+
+/**
+ * remove line in text
+ * @param txt > text pointer
+ * @param pos > line in text
+ */
 void remove_line_text(text *txt, int pos);
+
+/**
+ * remove all line that contains str
+ * @param txt > text pointer
+ * @param str > string
+ */
 void remove_line_text_string(text *txt, string str);
+
+/**
+ * add char at end str
+ * @param str > string pointer
+ * @param c > char to add
+ */
 void add_char_str(string *str, char c);
+
+/**
+ * add char at end str
+ * @param str > check in this str
+ * @param contain > check this str
+ * @return first position of contain in str
+ * if str does not contain contain the return -1
+ */
 int str_contain_pos(string str1, string contain);
+
+/**
+ * remove first char in str
+ * @param str > string pointer
+ */
 void str_rm_fchar(string *str);
+
+/**
+ * remove char in str at specific pos
+ * @param str > string pointer
+ * @param pos > position of char
+ */
 void str_rm_pchar(string *str, int pos);
+
+/**
+ * remove all occurences of string in text
+ * @param txt > text
+ * @param str > string to remove
+ */
 void remove_all_ostr_text(text txt, string str);
+
+/**
+ * replace char in text
+ * @param txt > text
+ * @param target > char to replace
+ * @param replace > char want
+ */
 void replace_char_text(text txt, char target, char replace);
+
+/**
+ * replace char in string
+ * @param str > string
+ * @param target > char to replace
+ * @param replace > char want
+ */
 void replace_char_str(string str, char target, char replace);
+
+/**
+ * change text to string
+ * @param split > text
+ * @param separator > string to place between line (Nullable)
+ * @return string result
+ */
 string text_to_str(text split, string separator);
+
+/**
+ * insert string "to_insert" in string "str" at specific position
+ * @param str > string pointer
+ * @param to_insert > string to insert in string str
+ * @param pos > position in string
+ */
 void insert_str_s(string *str, string to_insert, int pos);
+
+/**
+ * number of empty line in text
+ * @param str > text
+ * @return int with number of empty line in text
+ */
 int nb_o_split(text str);
+
+/**
+ * remove all occurences of "to_rem" in string "str"
+ * @param str > pointer string
+ * @param to_rem > string to remove
+ */
 void remove_ostr_str(string *str, string to_rem);
+
+/**
+ * display string in output 1
+ * @param str > string
+ */
 void my_putstr(string str);
+
+/**
+ * display string in output 2
+ * @param str > string
+ */
 void my_putstr_err(string str);
+
+/**
+ * display string in specific output
+ * @param str > string
+ * @param fd > output to display
+ */
 ssize_t my_putstr_fd(string str, int fd);
 
 #endif /* !MYSTR_H_ */
