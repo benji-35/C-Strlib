85a54cdd1df057c1687b1dcb18bc6e978d2dfc18
benji-35 <benjamin.delvert@epitech.eu>
2021-04-14 10:38:17 +0200

    ADD : functionnal split sstr

--- mylibstr/split_str_str.c
+++ mylibstr/split_str_str.c
@@ -23,9 +23,10 @@ static int nb_ssplit(string str, string splitter)
     return (result);
 }
 
-static int ssize_before_splitter(string str, string splitter, int curr_s)
+static int ssize_before_splitter(string str, string splitter)
 {
     int i = 0;
+    int size = length(str);
     int size_s = length(splitter);
 
     while (str[i] != 0) {
@@ -33,14 +34,23 @@ static int ssize_before_splitter(string str, string splitter, int curr_s)
             break;
         i++;
     }
-    return (i - size_s);
+    if (i > size)
+        i = size;
+    return (i);
 }
 
-static void complete_sstr_split(string str, int size, string result, int i)
+static void complete_sstr_split(string str, string split, string *res, int *i)
 {
+    int size_s = length(split);
+    int size = ssize_before_splitter(str, split);
+
+    *res = malloc(sizeof(char) * (size + 1));
+    if (*res == NULL)
+        return;
     for (int i = 0; i < size; i++)
-        result[i] = str[i];
-    result[size] = 0;
+        (*res)[i] = str[i];
+    (*res)[size] = 0;
+    *i = *i + size + size_s - 1;
 }
 
 text split_sstr(string str, string splitter)
@@ -50,11 +60,10 @@ text split_sstr(string str, string splitter)
     int size_s = length(splitter);
     int nb_split = nb_ssplit(str, splitter);
     text split = malloc(sizeof(string) * (nb_split + 2));
-    
+
     for (int i = 0; i < size; i++) {
-        split[curr] = concat_str(NULL, "oui");
+        complete_sstr_split(str + i, splitter, &split[curr], &i);
         curr++;
-        i+=size_s;
     }
     split[nb_split + 1] = NULL;
     return (split);
